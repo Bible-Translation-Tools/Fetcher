125646dd715281949d22c9774e406c16e930fa4d
DJ <djpric91@gmail.com>
2020-06-29 13:25:32 -0400

    interface stub and initial TODOs

---
+++ src/main/kotlin/org/bibletranslationtools/fetcher/FileSystemReader.kt
@@ -0,0 +1,8 @@
+package org.bibletranslationtools.fetcher
+
+interface FileSystemReader {
+    // TODO: what are the languages?
+    // TODO: what file types are supported?
+    // TODO: what books are there for a given language?
+    // TODO: what chapters are there for a given language, book, and file type?
+}
