9df127a87d58285af13a538f62cd0e41ae1f8180
DJ <donald.price077@topper.wku.edu>
2020-06-29 14:55:05 -0400

    function to get language by its language code

--- src/main/kotlin/org/bibletranslationtools/fetcher/Language.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/Language.kt
@@ -1,3 +1,14 @@
 package org.bibletranslationtools.fetcher
 
-data class Language(val languageCode: String, val anglicizedName: String, val localizedName: String)
+data class Language(
+    val languageCode: String,
+    val anglicizedName: String,
+    val localizedName: String
+) {
+    companion object {
+        fun getLanguageByLanguageCode(languageCode: String): Language? {
+            // TODO: implement this using langnames.json
+            return null
+        }
+    }
+}
