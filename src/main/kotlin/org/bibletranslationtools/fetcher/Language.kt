d35666218c64a6fe9f7c64c33755ef19f05f4314
DJ <donald.price077@topper.wku.edu>
2020-06-29 15:49:38 -0400

    dummy code for get languages for now

--- src/main/kotlin/org/bibletranslationtools/fetcher/Language.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/Language.kt
@@ -7,8 +7,13 @@ data class Language(
 ) {
     companion object {
         fun getLanguageByLanguageCode(languageCode: String): Language? {
-            // TODO: implement this using langnames.json
-            return null
+            // TODO: implement this using API to get languages
+            return when (languageCode) {
+                "en" -> Language("en", "English", "English")
+                "fr" -> Language("fr", "French", "français, langue française")
+                "vi" -> Language("vi", "Vietnamese", "Ti\u1ebfng Vi\u1ec7t")
+                else -> null
+            }
         }
     }
 }
