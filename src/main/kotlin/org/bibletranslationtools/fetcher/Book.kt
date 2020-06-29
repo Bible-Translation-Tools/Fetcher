4f49ffbbc277c527da5bcc29839ce08474a6a673
DJ <donald.price077@topper.wku.edu>
2020-06-29 15:53:47 -0400

    need a language to get a proper book with localized and anglicized names

--- src/main/kotlin/org/bibletranslationtools/fetcher/Book.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/Book.kt
@@ -2,6 +2,7 @@ package org.bibletranslationtools.fetcher
 
 data class Book(
     val id: String,
+    val language: Language,
     val anglicizedName: String,
     val localizedName: String,
     val bookNumber: Int
