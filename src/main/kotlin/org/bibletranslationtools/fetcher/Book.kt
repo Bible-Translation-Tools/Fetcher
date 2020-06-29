8ece7e61c67ba5bd8ed21571a6c56e6840f66544
DJ <donald.price077@topper.wku.edu>
2020-06-29 14:54:54 -0400

    function to get a book by its id

--- src/main/kotlin/org/bibletranslationtools/fetcher/Book.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/Book.kt
@@ -5,4 +5,11 @@ data class Book(
     val anglicizedName: String,
     val localizedName: String,
     val bookNumber: Int
-)
+) {
+    companion object {
+        fun getBookById(id: String): Book? {
+            // TODO: implement this using whatever book catalog we end up having
+            return null
+        }
+    }
+}
