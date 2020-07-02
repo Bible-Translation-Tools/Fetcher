3357b6a6d4c7ae4dea5dd38a1768fb9625d0b80d
DJ <donald.price077@topper.wku.edu>
2020-07-02 15:13:25 -0400

    rename repository and source audio languages file

--- src/main/kotlin/org/bibletranslationtools/fetcher/application/SourceAudioRepository.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/application/SourceAudioRepository.kt
@@ -4,9 +4,9 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.streams.toList
 import org.bibletranslationtools.fetcher.data.Language
-import org.bibletranslationtools.fetcher.domain.LanguageRepository
+import org.bibletranslationtools.fetcher.domain.LanguageCatalog
 
-class SourceAudioLanguages(private val languageRepository: LanguageRepository) {
+class SourceAudioLanguages(private val languageRepository: LanguageCatalog) {
 
     private val sourceAudioRoot = "/SourceAudio"
 
