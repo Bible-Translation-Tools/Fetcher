6ef3567e711657a85c14223f2a3f4ce88d28f474
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-07-07 11:08:33 -0400

    Change wildcard import to specific one. Remove CORS

--- src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
@@ -1,21 +1,15 @@
 package org.bibletranslationtools.fetcher.web
 
-import dev.jbs.ktor.thymeleaf.*
+import dev.jbs.ktor.thymeleaf.Thymeleaf
 import io.ktor.application.Application
 import io.ktor.application.install
-import io.ktor.features.CORS
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.HttpHeaders
 import io.ktor.routing.Routing
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
 fun Application.appModule() {
     install(DefaultHeaders)
-    install(CORS) {
-        anyHost()
-        header(HttpHeaders.AccessControlAllowOrigin)
-    }
     install(Thymeleaf) {
         setTemplateResolver(ClassLoaderTemplateResolver().apply {
             prefix = "templates/"
