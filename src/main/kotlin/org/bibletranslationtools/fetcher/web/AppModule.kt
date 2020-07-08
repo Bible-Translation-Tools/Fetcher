26d1dd5d88ad93e8d26ddd2eb6647c716f767ccd
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-07-08 09:48:04 -0400

    Set up ktor - appModule (#13)
    * set up ktor + customized thymeleaf
    
    libs/thymeleaf-1.0-SNAPSHOT.jar
    
    * Change wildcard import to specific one. Remove CORS
    
    * Merge branch 'dev' into tony-setup-ktor-server
    
    * add entry point: embeddedServer
    
    * name change
    
    * remove old main
    
    * remove unused val

---
+++ src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
@@ -0,0 +1,23 @@
+package org.bibletranslationtools.fetcher.web
+
+import dev.jbs.ktor.thymeleaf.Thymeleaf
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.routing.Routing
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+
+fun Application.appModule() {
+    install(DefaultHeaders)
+    install(Thymeleaf) {
+        setTemplateResolver(ClassLoaderTemplateResolver().apply {
+            prefix = "templates/"
+            suffix = ".html"
+            characterEncoding = "utf-8"
+        })
+    }
+    install(CallLogging)
+    install(Routing) {
+    }
+}
