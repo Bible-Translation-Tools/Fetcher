50fbc1ffefb0a353b6d507de3ecbe897e87ac820
Hoang Anh <hoanganhtran1998@gmail.com>
2020-07-07 22:28:20 -0400

    update test with mock

--- src/test/kotlin/org/bibletranslationtools/fetcher/RetrieveFileTypesTest.kt
+++ src/test/kotlin/org/bibletranslationtools/fetcher/RetrieveFileTypesTest.kt
@@ -1,27 +1,27 @@
 package org.bibletranslationtools.fetcher
 
+import org.bibletranslationtools.fetcher.data.FileType
+import org.bibletranslationtools.fetcher.repository.FileTypeCatalog
 import org.bibletranslationtools.fetcher.repository.FileTypeRepository
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
 class RetrieveFileTypesTest {
-    private val expectedFileTypes = setOf("tr", "mp3")
+    private val expectedFileTypes = listOf(
+        FileType("tr", "", "", ""),
+        FileType("mp3", "", "", "")
+    )
 
     @Test
-    fun `getFileTypes_implementationTest`() {
-        val types = FileTypeRepository().getFileTypes().map { it.type }
+    fun `getFileTypes_withMock`() {
+        val mockCatalog = mock(FileTypeCatalog::class.java)
+        `when`(mockCatalog.getFileTypes()).thenReturn(expectedFileTypes)
 
-        assertEquals(expectedFileTypes, types.toHashSet())
+        val types = mockCatalog.getFileTypes().map { it.type }
+        val actualTypes = FileTypeRepository().getFileTypes().map { it.type }
+
+        assertEquals(types.toHashSet(), actualTypes.toHashSet())
     }
-
-//    @Test
-//    fun `getFileTypes_interfaceMockingTest`() {
-//        val mockCatalog = mock(FileTypeCatalog::class.java)
-//        `when`(mockCatalog.getFileTypes()).thenReturn(FileTypeRepository().getFileTypes())
-//
-//        val types = mockCatalog.getFileTypes().map { it.type }
-//        verify(mockCatalog).getFileTypes()
-//
-//        assertEquals(expectedFileTypes, types.toHashSet())
-//    }
 }
