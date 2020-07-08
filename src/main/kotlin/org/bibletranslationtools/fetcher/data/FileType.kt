a90f5cc9a7e213ca1a0580ca71cd354fa18283ed
Hoang Anh <hoanganhtran1998@gmail.com>
2020-07-08 14:46:11 -0400

    change FileType data class using localized key instead

--- src/main/kotlin/org/bibletranslationtools/fetcher/data/FileType.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/data/FileType.kt
@@ -2,7 +2,7 @@ package org.bibletranslationtools.fetcher.data
 
 data class FileType(
     val type: String,
-    val title: String,
-    val description: String,
+    val titleKey: String,
+    val descriptionKey: String,
     val resourceDir: String
 )
