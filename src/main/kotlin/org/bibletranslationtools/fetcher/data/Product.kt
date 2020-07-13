b47e4e7386478ac8119c3ffd683c099cde0f7a8f
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-07-13 16:23:56 -0400

    Refactor (#15)
    change Product icon: File to iconUrl: String
    
    move impl file to correct package

--- src/main/kotlin/org/bibletranslationtools/fetcher/data/Product.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/data/Product.kt
@@ -1,10 +1,8 @@
 package org.bibletranslationtools.fetcher.data
 
-import java.io.File
-
 data class Product(
     val slug: String,
     val titleKey: String,
     val descriptionKey: String,
-    val icon: File
+    val iconUrl: String
 )
--- src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/ProductCatalogImpl.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/ProductCatalogImpl.kt
@@ -1,4 +1,4 @@
-package org.bibletranslationtools.fetcher.repository.implementations
+package org.bibletranslationtools.fetcher.impl.repository
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -9,14 +9,6 @@ import org.bibletranslationtools.fetcher.repository.ProductCatalog
 
 class ProductCatalogImpl : ProductCatalog {
     private val productCatalogFileName = "product_catalog.json"
-
-    private data class ProductSchema(
-        val slug: String,
-        val titleKey: String,
-        val descriptionKey: String,
-        val iconUrl: String
-    )
-
     private val products: List<Product> = parseCatalog()
 
     override fun getAll(): List<Product> = this.products
@@ -29,15 +21,7 @@ class ProductCatalogImpl : ProductCatalog {
             return listOf()
         }
 
-        val productsFromSchema: List<ProductSchema> = jacksonObjectMapper().readValue(jsonFileTypes)
-        return productsFromSchema.map {
-            Product(
-                it.slug,
-                it.titleKey,
-                it.descriptionKey,
-                File(it.iconUrl)
-            )
-        }
+        return jacksonObjectMapper().readValue(jsonFileTypes)
     }
 
     @Throws(FileNotFoundException::class)
