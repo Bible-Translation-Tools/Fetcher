2a94e40e8f5619d4a4206f3d74e5b825faeb51da
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-07-13 15:36:23 -0400

    move files to correct package

--- src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/ProductCatalogImpl.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/ProductCatalogImpl.kt
@@ -1,4 +1,4 @@
-package org.bibletranslationtools.fetcher.repository.implementations
+package org.bibletranslationtools.fetcher.impl.repository
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -22,14 +22,14 @@ class ProductCatalogImpl : ProductCatalog {
     override fun getAll(): List<Product> = this.products
 
     private fun parseCatalog(): List<Product> {
-        val jsonFileTypes: String = try {
+        val jsonProducts: String = try {
             val productsFile = getProductCatalogFile()
             productsFile.readText()
         } catch (e: FileNotFoundException) {
             return listOf()
         }
 
-        val productsFromSchema: List<ProductSchema> = jacksonObjectMapper().readValue(jsonFileTypes)
+        val productsFromSchema: List<ProductSchema> = jacksonObjectMapper().readValue(jsonProducts)
         return productsFromSchema.map {
             Product(
                 it.slug,
