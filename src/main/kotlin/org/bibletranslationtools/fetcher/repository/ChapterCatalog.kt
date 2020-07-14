7aa3699e756d14d8fc675397a86c4e8281670575
DJ <djpric91@gmail.com>
2020-07-14 09:27:39 -0400

    chapter catalog and repo interfaces

---
+++ src/main/kotlin/org/bibletranslationtools/fetcher/repository/ChapterCatalog.kt
@@ -0,0 +1,9 @@
+package org.bibletranslationtools.fetcher.repository
+
+import org.bibletranslationtools.fetcher.data.Book
+import org.bibletranslationtools.fetcher.data.Chapter
+import org.bibletranslationtools.fetcher.data.Language
+
+interface ChapterCatalog {
+    fun getChapterCount(language: Language, book: Book): Int
+}
