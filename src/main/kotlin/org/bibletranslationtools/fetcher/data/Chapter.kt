d9ad1fb2d5c159954c54d30b53d3f232f2eb7ab7
DJ <djpric91@gmail.com>
2020-07-15 10:25:37 -0400

    refactor chapter entity object

--- src/main/kotlin/org/bibletranslationtools/fetcher/data/Chapter.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/data/Chapter.kt
@@ -3,8 +3,8 @@ package org.bibletranslationtools.fetcher.data
 import java.io.File
 
 data class Chapter(
-    val index: Int,
-    val chapterFile: File?
+    val number: Int,
+    val file: File?
 ) {
-    fun hasDownload(): Boolean = chapterFile != null
+    fun hasDownload(): Boolean = file != null
 }
