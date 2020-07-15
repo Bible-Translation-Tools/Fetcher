3a90b46f99814044585e53f3021786574fce5b62
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-07-15 16:09:01 -0400

    add intercept for blocking the pipeline on fatal exception

--- src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
@@ -2,9 +2,12 @@ package org.bibletranslationtools.fetcher.web
 
 import dev.jbs.ktor.thymeleaf.Thymeleaf
 import io.ktor.application.Application
+import io.ktor.application.ApplicationCallPipeline
+import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
+import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
@@ -18,6 +21,9 @@ fun Application.appModule() {
         })
     }
     install(CallLogging)
+    intercept(ApplicationCallPipeline.Setup) {
+        // display error page here and terminate the pipeline if fatal exception occurs
+    }
     install(Routing) {
     }
 }
