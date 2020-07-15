c8a25ddaaf57f9d0e640a8671c3732942a585285
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-07-15 16:09:45 -0400

    remove imports

--- src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
@@ -3,11 +3,9 @@ package org.bibletranslationtools.fetcher.web
 import dev.jbs.ktor.thymeleaf.Thymeleaf
 import io.ktor.application.Application
 import io.ktor.application.ApplicationCallPipeline
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
-import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
