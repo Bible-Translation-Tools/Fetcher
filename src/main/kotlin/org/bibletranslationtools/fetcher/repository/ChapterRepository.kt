383f1fce1fedd561c2bb9d28acdf7fbc64f0d306
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-07-17 17:08:30 -0400

    remove ChapterRepo
    since the use case now will use Catalog and Storage Access for chapter retrieval

--- src/main/kotlin/org/bibletranslationtools/fetcher/repository/ChapterRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.bibletranslationtools.fetcher.repository
-
-import org.bibletranslationtools.fetcher.data.Chapter
-
-interface ChapterRepository {
-    fun getChapters(languageCode: String, bookSlug: String): List<Chapter>
-}
