0923deab66def2c0d1dc9ae0b38fccdad654e2b6
DJ <donald.price077@topper.wku.edu>
2020-10-16 07:27:18 -0500

    add temporary placeholder for orature file type

--- src/main/kotlin/org/bibletranslationtools/fetcher/usecase/ProductFileExtension.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/usecase/ProductFileExtension.kt
@@ -4,7 +4,8 @@ import java.lang.IllegalArgumentException
 
 enum class ProductFileExtension(val fileType: String) {
     MP3("mp3"),
-    BTTR("tr");
+    BTTR("tr"),
+    ORATURE("zip"); // TODO: I don't think this filetype is accurate...
 
     companion object {
         fun getType(productSlug: String): ProductFileExtension? {
