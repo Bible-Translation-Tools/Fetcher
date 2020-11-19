d7005d3306ac2d93468611c931f36dd4e8e50b6d
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-11-19 14:07:37 -0500

    change env var name

--- fetcher-pipeline/entrypoint.sh
+++ fetcher-pipeline/entrypoint.sh
@@ -3,6 +3,6 @@
 cd /fetcher-pipeline || exit
 
 pm2 start "python3.8 app.py -i /content -t -v -hr 0 -mn 0" --name fetcher-pipeline
-pm2 start "python3.8 directorycleaner.py -t -hr $UPDATE_REPO_HOUR -mn $UPDATE_REPO_MINUTE" --name directory-cleaner
+pm2 start "python3.8 directorycleaner.py -t -hr $DIR_CLEANUP_HOUR -mn $DIR_CLEANUP_MINUTE" --name directory-cleaner
 
 pm2 logs
