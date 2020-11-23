fd02f69d9ea7c79abb6348c5efdc1a7c6d1e5c20
DJ <donald.price077@topper.wku.edu>
2020-11-23 10:06:18 -0600

    read unfolding word json file

--- fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/UnfoldingWordHeartLanguagesCatalog.kt
+++ fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/UnfoldingWordHeartLanguagesCatalog.kt
@@ -2,9 +2,13 @@ package org.bibletranslationtools.fetcher.impl.repository
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.sun.org.slf4j.internal.LoggerFactory
 import org.bibletranslationtools.fetcher.data.Language
 import org.bibletranslationtools.fetcher.repository.LanguageCatalog
+import java.io.FileNotFoundException
+import java.io.InputStream
 
 const val UW_LANGUAGE_CODE_ID = "lc"
 const val UW_ANGLICIZED_NAME_ID = "ang"
@@ -27,10 +31,26 @@ class UnfoldingWordHeartLanguagesCatalog : LanguageCatalog {
 
     override fun getLanguage(code: String): Language? = this.languageList.firstOrNull { it.code == code }
 
+    @Throws(FileNotFoundException::class)
     private fun parseCatalog(): List<Language> {
-        
+        val jsonProducts: InputStream = try {
+            getLanguageCatalogFile()
+        } catch (e: FileNotFoundException) {
+            logger.error("$unfoldingWordLanguageFileName not found in resources.", e)
+            throw e // crash on fatal exception: critical resource not found
+        }
 
-        return listOf()
+        return jacksonObjectMapper().readValue(jsonProducts)
+    }
+
+    @Throws(FileNotFoundException::class)
+    private fun getLanguageCatalogFile(): InputStream {
+        val catalogFileStream = javaClass.getResourceAsStream(unfoldingWordLanguageFileName)
+        if (catalogFileStream == null) {
+            throw FileNotFoundException()
+        }
+
+        return catalogFileStream
     }
 
 }
