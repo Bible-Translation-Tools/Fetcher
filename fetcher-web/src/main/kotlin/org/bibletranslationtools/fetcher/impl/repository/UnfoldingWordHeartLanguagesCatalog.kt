9b4ab8f029bfa7e317d7906967db2bb0792e385e
DJ <donald.price077@topper.wku.edu>
2020-11-23 13:59:27 -0600

    fix typos and read heart languages from file into list

--- fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/UnfoldingWordHeartLanguagesCatalog.kt
+++ fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/UnfoldingWordHeartLanguagesCatalog.kt
@@ -2,13 +2,15 @@ package org.bibletranslationtools.fetcher.impl.repository
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.MappingIterator
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.sun.org.slf4j.internal.LoggerFactory
 import org.bibletranslationtools.fetcher.data.Language
 import org.bibletranslationtools.fetcher.repository.LanguageCatalog
 import java.io.FileNotFoundException
 import java.io.InputStream
+import org.slf4j.LoggerFactory
 
 const val UW_LANGUAGE_CODE_ID = "lc"
 const val UW_ANGLICIZED_NAME_ID = "ang"
@@ -17,7 +19,7 @@ const val UW_LOCALIZED_NAME_ID = "ln"
 class UnfoldingWordHeartLanguagesCatalog : LanguageCatalog {
 
     @JsonIgnoreProperties(ignoreUnknown = true)
-    private data class UnfoldingWordGatewayLanguage(
+    private data class UnfoldingWordHeartLanguage(
         @JsonProperty(UW_LANGUAGE_CODE_ID) val code: String,
         @JsonProperty(UW_ANGLICIZED_NAME_ID) val anglicizedName: String,
         @JsonProperty(UW_LOCALIZED_NAME_ID) val localizedName: String
@@ -33,14 +35,24 @@ class UnfoldingWordHeartLanguagesCatalog : LanguageCatalog {
 
     @Throws(FileNotFoundException::class)
     private fun parseCatalog(): List<Language> {
-        val jsonProducts: InputStream = try {
+        val jsonLanguages: InputStream = try {
             getLanguageCatalogFile()
         } catch (e: FileNotFoundException) {
             logger.error("$unfoldingWordLanguageFileName not found in resources.", e)
             throw e // crash on fatal exception: critical resource not found
         }
 
-        return jacksonObjectMapper().readValue(jsonProducts)
+        val mapper = jacksonObjectMapper().registerModule(KotlinModule())
+        val languagesIterator: MappingIterator<UnfoldingWordHeartLanguage> = mapper.readerFor(
+            UnfoldingWordHeartLanguage::class.java
+        ).readValues(jsonLanguages)
+
+        val languageList = mutableListOf<Language>()
+        languagesIterator.forEach {
+            languageList.add(Language(it.code, it.anglicizedName, it.localizedName))
+        }
+
+        return languageList
     }
 
     @Throws(FileNotFoundException::class)
