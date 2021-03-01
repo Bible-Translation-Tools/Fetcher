8419b59a193d531a57e35f31a770f123e23d4c10
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-03-01 11:23:55 -0500

    rename more env var

--- fetcher-pipeline/entrypoint.sh
+++ fetcher-pipeline/entrypoint.sh
@@ -3,7 +3,7 @@
 cd /fetcher-pipeline || exit
 
 # Start scripts
-pm2 start "python app.py -i /content -t -v -hr $PIPELINE_HOUR -mn $PIPELINE_MINUTE" --name fetcher-pipeline
+pm2 start "python app.py -i /content -t -v -hr $PIPELINE_FREQUENCY_HOUR -mn $PIPELINE_FREQUENCY_MINUTE" --name fetcher-pipeline
 pm2 start "python directorycleaner.py -t -hr $DIR_CLEANUP_FREQUENCY_HOUR -mn $DIR_CLEANUP_FREQUENCY_MINUTE" --name directory-cleaner
 pm2 start "python update_repo.py -t -v -hr $UPDATE_REPO_FREQUENCY_HOUR -mn $UPDATE_REPO_FREQUENCY_MINUTE" --name update-rc-respository
 
