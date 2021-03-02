5c1cc1b00ae5c6cf530f7899f547861d11ce9567
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-03-02 10:18:24 -0500

    rename for better comprehension

--- fetcher-pipeline/entrypoint.sh
+++ fetcher-pipeline/entrypoint.sh
@@ -3,9 +3,9 @@
 cd /fetcher-pipeline || exit
 
 # Start scripts
-pm2 start "python app.py -i /content -t -v -hr $PIPELINE_HOUR -mn $PIPELINE_MINUTE" --name fetcher-pipeline
-pm2 start "python directorycleaner.py -t -hr $DIR_CLEANUP_HOUR -mn $DIR_CLEANUP_MINUTE" --name directory-cleaner
-pm2 start "python update_repo.py -t -v -hr $UPDATE_REPO_HOUR -mn $UPDATE_REPO_MINUTE" --name update-rc-respository
+pm2 start "python app.py -i /content -t -v -hr $PIPELINE_FREQUENCY_HOUR -mn $PIPELINE_FREQUENCY_MINUTE" --name fetcher-pipeline
+pm2 start "python directorycleaner.py -t -hr $DIR_CLEANUP_FREQUENCY_HOUR -mn $DIR_CLEANUP_FREQUENCY_MINUTE" --name directory-cleaner
+pm2 start "python update_repo.py -t -v -hr $UPDATE_REPO_FREQUENCY_HOUR -mn $UPDATE_REPO_FREQUENCY_MINUTE" --name update-rc-respository
 
 # Output log to stdout
 pm2 logs
