8d61bba6f35e917d22647e09d19a9d78ad4d0f6b
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-03-09 08:13:57 -0500

    add test for zipDirectory in RCUtil

--- fetcher-web/src/test/kotlin/org/bibletranslationtools/fetcher/RCUtilTest.kt
+++ fetcher-web/src/test/kotlin/org/bibletranslationtools/fetcher/RCUtilTest.kt
@@ -7,12 +7,13 @@ import java.io.FileNotFoundException
 import java.net.URL
 import org.bibletranslationtools.fetcher.impl.repository.RCUtils
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.slf4j.LoggerFactory
 import org.wycliffeassociates.rcmediadownloader.data.MediaType
+import org.wycliffeassociates.resourcecontainer.ResourceContainer
 
 class RCUtilTest {
-    private val rcFileName = "titus_test.zip"
     private val testCaseFileName = "VerifyChapterExists_TestCases.json"
     private val logger = LoggerFactory.getLogger(javaClass)
 
@@ -25,7 +26,7 @@ class RCUtilTest {
 
     @Test
     fun testVerifyChapterExists() {
-        val rcFile = getTestRCFile()
+        val rcFile = getTestRCFile("titus_test.zip")
         val testCases = getVerifyChapterExistsTestCase()
         for (case in testCases) {
             val mediaTypes = case.mediaTypes.mapNotNull { MediaType.get(it) }
@@ -39,10 +40,26 @@ class RCUtilTest {
         }
     }
 
+    @Test
+    fun testZipDirectory() {
+        val repoRC = getTestRCFile("en_ulb")
+        val zipRC = createTempDir().resolve("rc.zip")
+        val result = RCUtils.zipDirectory(repoRC, zipRC)
+
+        assertTrue(
+            "Cannot zip directory from $repoRC to $zipRC",
+            result
+        )
+        assertTrue(zipRC.isFile)
+        ResourceContainer.load(zipRC).close() // valid resource container
+
+        zipRC.parentFile.deleteRecursively()
+    }
+
     @Throws(FileNotFoundException::class)
-    private fun getTestRCFile(): File {
-        val rcFilePath = javaClass.classLoader.getResource(rcFileName)
-            ?: throw(FileNotFoundException("Test resource not found: $rcFileName"))
+    private fun getTestRCFile(name: String): File {
+        val rcFilePath = javaClass.classLoader.getResource(name)
+            ?: throw(FileNotFoundException("Test resource not found: $name"))
         return File(rcFilePath.file)
     }
 
