b81a843e2243b4fa9434426149e39eb6a349b7f2
danparisd <43418833+danparisd@users.noreply.github.com>
2021-03-18 14:12:29 -0400

    add additional params (#123)
    * add additional params
    
    * add params to dockerstack env

--- fetcher-pipeline/entrypoint.sh
+++ fetcher-pipeline/entrypoint.sh
@@ -3,9 +3,9 @@
 cd /fetcher-pipeline || exit
 
 # Start scripts
-pm2 start "python app.py -i /content -t -v -hr $PIPELINE_FREQUENCY_HOUR -mn $PIPELINE_FREQUENCY_MINUTE" --name fetcher-pipeline
-pm2 start "python directorycleaner.py -t -hr $DIR_CLEANUP_FREQUENCY_HOUR -mn $DIR_CLEANUP_FREQUENCY_MINUTE" --name directory-cleaner
-pm2 start "python update_repo.py -t -v -hr $UPDATE_REPO_FREQUENCY_HOUR -mn $UPDATE_REPO_FREQUENCY_MINUTE" --name update-rc-respository
+pm2 start "python app.py -i /content ${ADDITIONAL_PARAMS_PIPELINE} -hr $PIPELINE_FREQUENCY_HOUR -mn $PIPELINE_FREQUENCY_MINUTE" --name fetcher-pipeline
+pm2 start "python directorycleaner.py ${ADDITIONAL_PARAMS_DIRECTORY_CLEANER} -hr $DIR_CLEANUP_FREQUENCY_HOUR -mn $DIR_CLEANUP_FREQUENCY_MINUTE" --name directory-cleaner
+pm2 start "python update_repo.py ${ADDITIONAL_PARAMS_UPDATE_REPO} -hr $UPDATE_REPO_FREQUENCY_HOUR -mn $UPDATE_REPO_FREQUENCY_MINUTE" --name update-rc-respository
 
 # Output log to stdout
 pm2 logs
