5e85f2db49e9cf212657b23bd50cfb6053d2bbeb
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-04-12 15:32:56 -0400

    update to prod url for testing

--- fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/config/DevEnvironmentConfig.kt
+++ fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/config/DevEnvironmentConfig.kt
@@ -4,5 +4,5 @@ package org.bibletranslationtools.fetcher.config
  * For Development only
  */
 class DevEnvironmentConfig : EnvironmentConfig() {
-    override val CDN_BASE_URL: String = "http:/audiobieldev-content.walink.org:8081/content"
+    override val CDN_BASE_URL: String = "https://audio-content.bibleineverylanguage.org/content"
 }
