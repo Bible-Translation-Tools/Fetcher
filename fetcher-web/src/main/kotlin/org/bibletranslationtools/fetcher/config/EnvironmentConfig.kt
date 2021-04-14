66c9c8cb4a71b758ce309ad539f360403df295cb
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-04-14 16:53:54 -0400

    change cache refresh rate unit to minute

--- fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/config/EnvironmentConfig.kt
+++ fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/config/EnvironmentConfig.kt
@@ -4,7 +4,7 @@ open class EnvironmentConfig {
     open val CONTENT_ROOT_DIR: String = System.getenv("CONTENT_ROOT")
     open val CDN_BASE_URL: String = System.getenv("CDN_BASE_URL")
     open val CDN_BASE_RC_URL: String = System.getenv("CDN_BASE_RC_URL")
-    open val CACHE_REFRESH_HOURS: String = System.getenv("CACHE_REFRESH_TIME_HRS")
+    open val CACHE_REFRESH_MINUTES: String = System.getenv("CACHE_REFRESH_MINUTES")
     open val ORATURE_REPO_DIR: String = System.getenv("ORATURE_REPO_DIR")
     open val RC_OUTPUT_DIR: String = System.getenv("RC_TEMP_DIR")
 }
