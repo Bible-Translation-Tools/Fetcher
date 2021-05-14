ebf98f5c9d07b490ba5a4bdb2cbd4e57712f6fde
Tony Anh Tran <34975907+AnonymousWalker@users.noreply.github.com>
2021-05-14 15:13:55 -0400

    Use Zip4J for Orature file packaging (#138)

--- fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/RCUtils.kt
+++ fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/impl/repository/RCUtils.kt
@@ -1,12 +1,9 @@
 package org.bibletranslationtools.fetcher.impl.repository
 
 import java.io.File
-import java.net.URI
-import java.nio.file.FileSystems
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
 import java.util.zip.ZipFile
+import net.lingala.zip4j.ZipFile as Zip4J
+import net.lingala.zip4j.model.ZipParameters
 import org.bibletranslationtools.fetcher.data.Deliverable
 import org.wycliffeassociates.rcmediadownloader.data.MediaType
 import org.wycliffeassociates.resourcecontainer.ResourceContainer
@@ -58,35 +55,14 @@ object RCUtils {
 
     @Suppress("TooGenericExceptionCaught")
     fun zipDirectory(source: File, dest: File): Boolean {
-        if (!source.isDirectory || dest.exists()) {
-            return false
-        }
-
-        val env = mapOf("create" to "true")
-
-        val success = try {
-            val uri: URI = URI.create("jar:file:///${dest.invariantSeparatorsPath}")
-            FileSystems.newFileSystem(uri, env).use { zipFileSystem ->
-
-                source.walk().filterNot { it == source }.forEach {
-                    val fileFromSource = Paths.get(it.path)
-                    val pathInZipFile = zipFileSystem.getPath(
-                        it.path.removePrefix(source.path)
-                    )
-
-                    // copy into zip file
-                    Files.copy(
-                        fileFromSource, pathInZipFile,
-                        StandardCopyOption.REPLACE_EXISTING
-                    )
-                }
-            }
+        return try {
+            val options = ZipParameters()
+            options.isIncludeRootFolder = false
+            Zip4J(dest).addFolder(source, options)
             true
         } catch (ex: Exception) {
             false
         }
-
-        return success
     }
 
     private fun checkFromZipFile(rcFile: File, chapterPath: String): Boolean {
