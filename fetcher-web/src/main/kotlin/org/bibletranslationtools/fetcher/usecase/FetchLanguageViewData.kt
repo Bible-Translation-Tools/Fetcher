15059d5248d543adfced5f0d631fc73bb856103b
Test <murmax82@gmail.com>
2024-05-18 16:27:45 +0400

    add sorting comparator

--- fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/usecase/FetchLanguageViewData.kt
+++ fetcher-web/src/main/kotlin/org/bibletranslationtools/fetcher/usecase/FetchLanguageViewData.kt
@@ -14,40 +14,15 @@ class FetchLanguageViewData(
         private const val MATCHING_RESULT_TAKE = 20
     }
 
+    private val comparator = compareBy(LanguageViewData::isGateway)
+        .then(compareByDescending { it.url != null })
+
     fun getViewDataList(
         currentPath: String
     ): List<LanguageViewData> {
         val languages = languageRepo.getAll()
 
-        val listViewData = languages.sortedBy { it.isGateway }.map {
-            val available = storage.hasLanguageContent(it.code)
-
-            LanguageViewData(
-                code = it.code,
-                anglicizedName = it.anglicizedName,
-                localizedName = it.localizedName,
-                url = if (available) {
-                    "$currentPath/${it.code}"
-                } else {
-                    null
-                }
-            )
-        }
-
-        val availableLanguages = listViewData.filter { it.url != null }
-        val unavailableLanguages = listViewData.filter { it.url == null }
-
-        val allLanguages = availableLanguages + unavailableLanguages
-
-        return allLanguages.take(DISPLAY_ITEMS_LIMIT)
-    }
-
-    fun filterLanguages(
-        query: String,
-        currentPath: String,
-        currentIndex: Int = 0
-    ): List<LanguageViewData> {
-        val result = getMatchingLanguages(query, languageRepo.getAll().sortedBy { it.isGateway })
+        return languages
             .map {
                 val available = storage.hasLanguageContent(it.code)
 
@@ -55,6 +30,7 @@ class FetchLanguageViewData(
                     code = it.code,
                     anglicizedName = it.anglicizedName,
                     localizedName = it.localizedName,
+                    isGateway = it.isGateway,
                     url = if (available) {
                         "$currentPath/${it.code}"
                     } else {
@@ -62,13 +38,32 @@ class FetchLanguageViewData(
                     }
                 )
             }
+            .sortedWith(comparator)
+            .take(DISPLAY_ITEMS_LIMIT)
+    }
 
-        val availableLanguages = result.filter { it.url != null }
-        val unavailableLanguages = result.filter { it.url == null }
+    fun filterLanguages(
+        query: String,
+        currentPath: String,
+        currentIndex: Int = 0
+    ): List<LanguageViewData> {
+        return getMatchingLanguages(query, languageRepo.getAll())
+            .map {
+                val available = storage.hasLanguageContent(it.code)
 
-        val allLanguages = availableLanguages + unavailableLanguages
-
-        return allLanguages
+                LanguageViewData(
+                    code = it.code,
+                    anglicizedName = it.anglicizedName,
+                    localizedName = it.localizedName,
+                    isGateway = it.isGateway,
+                    url = if (available) {
+                        "$currentPath/${it.code}"
+                    } else {
+                        null
+                    }
+                )
+            }
+            .sortedWith(comparator)
             .drop(currentIndex)
             .take(DISPLAY_ITEMS_LIMIT)
     }
@@ -79,27 +74,23 @@ class FetchLanguageViewData(
     ): List<LanguageViewData> {
         if (currentIndex < 0) return listOf()
 
-        val listViewData = languageRepo.getAll().sortedBy { it.isGateway }.map {
-            val available = storage.hasLanguageContent(it.code)
+        return languageRepo.getAll()
+            .map {
+                val available = storage.hasLanguageContent(it.code)
 
-            LanguageViewData(
-                code = it.code,
-                anglicizedName = it.anglicizedName,
-                localizedName = it.localizedName,
-                url = if (available) {
-                    "$currentPath/${it.code}"
-                } else {
-                    null
-                }
-            )
-        }
-
-        val availableLanguages = listViewData.filter { it.url != null }
-        val unavailableLanguages = listViewData.filter { it.url == null }
-
-        val allLanguages = availableLanguages + unavailableLanguages
-
-        return allLanguages
+                LanguageViewData(
+                    code = it.code,
+                    anglicizedName = it.anglicizedName,
+                    localizedName = it.localizedName,
+                    isGateway = it.isGateway,
+                    url = if (available) {
+                        "$currentPath/${it.code}"
+                    } else {
+                        null
+                    }
+                )
+            }
+            .sortedWith(comparator)
             .drop(currentIndex + DISPLAY_ITEMS_LIMIT)
             .take(DISPLAY_ITEMS_LIMIT)
     }
