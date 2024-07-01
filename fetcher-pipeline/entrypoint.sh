ffd1c3426a70a6200238c3dbf7022fa3052cc61d
Will Kelly <67284402+wkelly17@users.noreply.github.com>
2024-07-01 12:47:47 -0500

    feature to send messages to messanger queue after each batch. (#206)
    * feature to send messages to messanger queue after each batch.
    
    * also edited docker files
    
    * add sh script to adjust to deploy on jenkins

--- fetcher-pipeline/entrypoint.sh
+++ fetcher-pipeline/entrypoint.sh
@@ -3,7 +3,8 @@
 cd /fetcher-pipeline || exit
 
 # Start scripts
-pm2 start "python app.py -i /content ${ADDITIONAL_PARAMS_PIPELINE} -hr $PIPELINE_FREQUENCY_HOUR -mn $PIPELINE_FREQUENCY_MINUTE" --name fetcher-pipeline
+pm2 start "python app.py -i /content ${ADDITIONAL_PARAMS_PIPELINE} -hr $PIPELINE_FREQUENCY_HOUR -mn $PIPELINE_FREQUENCY_MINUTE --queue_exclude $QUEUE_EXCLUDE_ARGS"  --name fetcher-pipeline
+
 pm2 start "python directorycleaner.py ${ADDITIONAL_PARAMS_DIRECTORY_CLEANER} -hr $DIR_CLEANUP_FREQUENCY_HOUR -mn $DIR_CLEANUP_FREQUENCY_MINUTE" --name directory-cleaner
 pm2 start "python update_repo.py ${ADDITIONAL_PARAMS_UPDATE_REPO} -hr $UPDATE_REPO_FREQUENCY_HOUR -mn $UPDATE_REPO_FREQUENCY_MINUTE" --name update-rc-respository
 
